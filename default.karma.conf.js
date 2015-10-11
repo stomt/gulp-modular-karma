6b399d701e6ab97ccd542bee31174b4589e6c606
Max Klenk <max@klenk.biz>
2015-10-11 15:00:51 +0200

    initial commit

---
+++ default.karma.conf.js
@@ -0,0 +1,89 @@
+// Karma configuration
+
+module.exports = function (config) {
+  config.set({
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
+
+    // testing framework to use (jasmine/mocha/qunit/...)
+    frameworks: ['jasmine'],
+
+    // list of files / patterns to load in the browser
+    files: [
+      // vendor
+      'dist/js/vendor*.js',
+      // test dependency
+      'app/bower_components/angular-mocks/angular-mocks.js',
+
+      // app
+      'dist/js/scripts*.js',
+
+      // tests
+      'app/components/**/*_test.js'
+    ],
+
+    // list of files / patterns to exclude
+    exclude: [],
+
+    // web server port
+    port: 9002,
+
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['PhantomJS'],
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false,
+
+
+    // Report Test Results
+    reporters: ['progress', 'junit', 'coverage'],
+
+    // the default configuration
+    junitReporter: {
+      outputDir: 'coverage/unit/',
+      outputFile: 'coverage/unit/unit-results.xml',
+      suite: ''
+    },
+
+    preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+
+      'dist/js/scripts*.js': ['sourcemap', 'coverage']
+    },
+
+    // optionally, configure the reporter
+    coverageReporter: {
+      reporters:[
+        {
+          type: 'html',
+          dir:'coverage/unit/'
+        },
+        {
+          type : 'cobertura',
+          dir : 'coverage/unit/',
+          file : 'coverage-results.xml'
+        }
+      ]
+    }
+  });
+};
---
+++ index.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var Server = require('karma').Server;
+var runSequence = require('run-sequence');
+
+module.exports = function(gulp, config) {
+
+  var configFile = config.dirname + '/karma.conf.js';
+  var browsers = ['PhantomJS', 'Firefox', 'Chrome'];
+
+  gulp.task('karma', function (done) {
+    new Server({
+      configFile: configFile,
+      singleRun: true
+    }, done).start();
+  });
+
+  gulp.task('karma:watch', function (done) {
+    new Server({
+      configFile: configFile,
+      singleRun: false,
+      autoWatch: true
+    }, done).start();
+  });
+
+  gulp.task('karma:all', function (done) {
+    new Server({
+      configFile: configFile,
+      browsers: browsers,
+      singleRun: true
+    }, done).start();
+  });
+
+  gulp.task('test', function () {
+    runSequence('build', 'karma');
+  });
+
+  gulp.task('test:all', function () {
+    runSequence('build', 'karma:all');
+  });
+
+  gulp.task('test:watch', function () {
+    runSequence('build', 'karma:watch');
+  });
+
+};
